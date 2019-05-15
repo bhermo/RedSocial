231129202c62e67b005d1ce2ad968b0ce4bd01bd
bhermo <braishermovazquez@gmail.com>
2019-05-15 12:31:42 +0200

    Fail on build.gradle(app)

--- app/src/main/java/com/example/redsocial/MapsActivity.java
+++ app/src/main/java/com/example/redsocial/MapsActivity.java
@@ -1,16 +1,66 @@
 package com.example.redsocial;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapsActivity extends AppCompatActivity {
 
-
+    private int MY_PERMISSIONS_REQUEST_READ_CONTACTS ;
+    private FusedLocationProviderClient mFusedLocationClient;
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        uploadLatLongFirebase();
+    }
+
+    private void uploadLatLongFirebase() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MapsActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+            return;
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+
+                        if (location != null) {
+                            Log.e("Latitude:", + location.getLatitude()+"Longitude:"+location.getLongitude());
+
+                            Map<String,Object> latlang = new HashMap<>();
+                            latlang.put("latitud",location.getLatitude());
+                            latlang.put("longitud",location.getLongitude());
+                            mDatabase.child("usuarios").push().setValue(latlang);
+                        }
+                    }
+                });
     }
 }
